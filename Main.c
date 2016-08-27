adf099d83161db0a517fa303f2b863fd97522bd1
Kurisu.Ti.Na <labman004@outlook.com>
2016-08-28 07:41:07 +0800

    update use case

--- Main.c
+++ Main.c
@@ -1,5 +1,5 @@
 ﻿#include "DynamicPage.h"
-extern int newgenerateNewPage(DPagePtr dpp, char **originalKeywords, char **newKeywords, int pairNum);
+extern int generateNewPage(DPagePtr dpp, char **originalKeywords, char **newKeywords, int pairNum);
 // use case
 int main(){
   char *headKey = "#<=";
@@ -8,7 +8,6 @@ int main(){
   char *originalKeywords[2] = { "测试", "test2test2" };
   char *newKeywords[2] = { "aaa", "bbb" };
   DPagePtr dpp = newDPage("D:\\s2.txt", headKey, tailKey);
-  //generateNewPage(dpp, originalKeywords, newKeywords, pairNum);
   generateNewPage(dpp, originalKeywords, newKeywords, pairNum);
   printf("%s\n", dpp->newPageBuf);
   printf("content len is : %d\n", dpp->contentLen);
